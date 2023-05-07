4330cc55e413529e5ee6c0c2c323cbcfe69542a0
luccazovedi <80693993+luccazovedi@users.noreply.github.com>
2023-05-07 17:21:04 -0300

    Versão Final

--- script.js
+++ script.js
@@ -16,3 +16,12 @@ filterButtons.forEach(button => {
     });
   });
 });
+
+function navbar() {
+  var x = document.getElementById("navbar");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
